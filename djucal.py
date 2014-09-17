0a25ef1a23dd9954674e23a17b42296caa161023
kjwon15 <kjwonmail@gmail.com>
2014-09-17 23:26:00 +0900

    Get timetables

--- djucal.py
+++ djucal.py
@@ -9,13 +9,26 @@ from lxml import html
 
 
 Schedule = namedtuple('Schedule', ('title', 'start', 'end', 'depart'))
+Timetable = namedtuple('Timetable', (
+    'grade', 'division', 'code', 'classcode', 'classtype', 'classname', 'score',
+    'time', 'minor', 'profname', 'times', 'maxstudents', 'available'))
 
 
 class DjuAgent(object):
     URL_LOGIN = 'http://intra.dju.kr/servlet/sys.syd.syd01Svl03'
     URL_SCHEDULE = ('http://intra.dju.kr/servlet/sys.syc.syc01Svl15'
                     '?pgm_id=W_SYS032PQ&pass_gbn=&dpt_ck=')
+    # TODO: documentation departcode.
+    URL_TIMETABLE = ('http://intra.dju.kr/myhtml/su/sue/schedule/'
+                     '{year}-{semester}{isbreak}-001'
+                     '-{departcode}-{category}.htm')
     DATE_FORMAT = '%Y-%m-%d %H-%M-%S'
+
+    TIMETABLE_CATEGORIES = {
+        'all': 0,
+        'liberal': 1,
+        'major': 2,
+    }
 
     def __init__(self, userid=None, userpw=None):
         cookiejar = cookielib.CookieJar()
@@ -69,3 +82,32 @@ class DjuAgent(object):
                 depart = tr.find('td[4]').text_content().strip()
 
                 yield Schedule(title, start, end, depart)
+
+    def get_timetables(self, year, semester, isbreak, departcode, category):
+        url = self.URL_TIMETABLE.format(
+            year=year, semester=semester, isbreak=isbreak,
+            departcode=departcode, category=category)
+        with closing(self.opener.open(url)) as fp:
+            content = fp.read()
+            tree = html.fromstring(content)
+            trs = tree.xpath('//table[3]/tr')[1:]
+
+            for tr in trs:
+                grade = tr.find('td[1]').text_content().strip()
+                division = tr.find('td[2]').text_content().strip()
+                code = tr.find('td[3]').text_content().strip()
+                classcode = tr.find('td[4]').text_content().strip()
+                classtype = tr.find('td[5]').text_content().strip()
+                classname = tr.find('td[6]').text_content().strip()
+                score = int(tr.find('td[7]').text_content().strip())
+                time = int(tr.find('td[8]').text_content().strip())
+                minor = tr.find('td[9]').text_content().strip()
+                profname = tr.find('td[10]').text_content().strip()
+                # FIXME: parse this to array
+                times = tr.find('td[11]').text_content().strip()
+                maxstudents = int(tr.find('td[12]').text_content().strip())
+                available = tr.find('td[13]').text_content().strip()
+
+                yield Timetable(grade, division, code, classcode, classtype,
+                                classname, score, time, minor, profname, times,
+                                maxstudents, available)
