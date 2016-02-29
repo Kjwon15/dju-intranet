14a62d687356b62c2ba887c366f828eb45443acd
kjwon15 <kjwonmail@gmail.com>
2016-02-29 15:06:26 +0900

    Release v0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 
 
-version = '0.2.0'
+version = '0.3.0'
 
 install_requires = [
     'lxml>=3.4.0',
